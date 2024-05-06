e82ca9fb79ed619018bb7b45f8be0f97d2fae1a5
Marcos Krul <marcoskrul2k@gmail.com>
2024-05-06 20:25:23 -0300

    feat(controllers): stock controller initial setup

---
+++ Controllers/StockController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TucaAPI.Data;
+
+namespace TucaAPI.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+        
+        public StockController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() {
+            var stocks = this.context.Stocks.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id) {
+            var stock = this.context.Stocks.Find(id);
+
+            if (stock == null) return NotFound();
+            
+            return Ok(stock);
+        }
+    }
+}
