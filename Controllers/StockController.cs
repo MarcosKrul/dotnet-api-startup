73a014961f1fe52fdc538ccaef088c1071496da3
Marcos Krul <marcoskrul2k@gmail.com>
2024-05-08 20:38:25 -0300

    feat(controlle): update stock

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -10,35 +10,58 @@ namespace TucaAPI.Controllers
     public class StockController : ControllerBase
     {
         private readonly ApplicationDBContext context;
-        
+
         public StockController(ApplicationDBContext context)
         {
             this.context = context;
         }
 
         [HttpGet]
-        public IActionResult GetAll() {
+        public IActionResult GetAll()
+        {
             var stocks = this.context.Stocks.ToList().Select(s => s.ToStockDto());
             return Ok(stocks);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute] int id) {
+        public IActionResult GetById([FromRoute] int id)
+        {
             var stock = this.context.Stocks.Find(id);
 
             if (stock == null) return NotFound();
-            
+
             return Ok(stock.ToStockDto());
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody] CreateStockRequestDto data) {
+        public IActionResult Create([FromBody] CreateStockRequestDto data)
+        {
             var stockModel = data.ToStockFromCreateDTO();
 
             this.context.Add(stockModel);
             this.context.SaveChanges();
 
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto data)
+        {
+            var stock = this.context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stock == null) return NotFound();
+
+            stock.Symbol = data.Symbol;
+            stock.CompanyName = data.CompanyName;
+            stock.Industry = data.Industry;
+            stock.LastDiv = data.LastDiv;
+            stock.Purchase = data.Purchase;
+            stock.MarketCap = data.MarketCap;
+
+            this.context.SaveChanges();
+
+            return Ok(stock.ToStockDto());
         }
     }
 }
