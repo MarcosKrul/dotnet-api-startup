d6e2094d586e1ba681a080ea02461bfa59115e1b
Marcos Krul <marcoskrul2k@gmail.com>
2024-06-16 00:28:27 -0300

    refact(extension): string refact error handling & to error key method

--- src/Utilities/Extensions/StringExtensions.cs
+++ src/Utilities/Extensions/StringExtensions.cs
@@ -1,5 +1,5 @@
+using System.Text;
 using TucaAPI.src.Common;
-using TucaAPI.src.Dtos.Common;
 using TucaAPI.src.Exceptions;
 
 namespace TucaAPI.src.Extensions
@@ -9,12 +9,40 @@ namespace TucaAPI.src.Extensions
         public static string GetNonNullable(this string? value)
         {
             if (value is null)
-                throw new AppException(StatusCodes.Status500InternalServerError, new ErrorApiResponse<string>
-                {
-                    Errors = [MessageKey.STRING_NON_NULLABLE]
-                });
+                throw new AppException(
+                    StatusCodes.Status500InternalServerError,
+                    MessageKey.STRING_NON_NULLABLE
+                );
 
             return value;
+        }
+
+        public static string ToErrorKeyFormat(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            StringBuilder result = new StringBuilder();
+            bool lastWasUpperCase = false;
+
+            foreach (char c in input)
+            {
+                if (char.IsUpper(c))
+                {
+                    if (!lastWasUpperCase && result.Length > 0)
+                        result.Append('_');
+
+                    result.Append(char.ToUpper(c));
+                    lastWasUpperCase = true;
+                }
+                else
+                {
+                    result.Append(char.ToUpper(c));
+                    lastWasUpperCase = false;
+                }
+            }
+
+            return result.ToString();
         }
     }
 }
