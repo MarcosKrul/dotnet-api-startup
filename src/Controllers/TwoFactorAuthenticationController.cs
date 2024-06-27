91d43fb4898f41f63da062ccb86fa886cdf0ab48
Marcos Krul <marcoskrul2k@gmail.com>
2024-06-26 21:17:17 -0300

    feat(controller): add two factor authentication controller

---
+++ src/Controllers/TwoFactorAuthenticationController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TucaAPI.src.Attributes;
+using TucaAPI.src.Dtos.Common;
+using TucaAPI.src.Services.TwoFactorAuthentication;
+using TucaAPI.src.Utilities.Extensions;
+
+namespace TucaAPI.src.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TwoFactorAuthenticationController : ControllerBase
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public TwoFactorAuthenticationController(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        [HttpGet("getGoogleAuthenticatorQRCode")]
+        [Authorize]
+        [ValidateModelState]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            using (var scope = this.serviceProvider.CreateScope())
+            {
+                var data = new UserAuthenticatedInfos();
+                data.AggregateUser(User);
+                var service =
+                    scope.ServiceProvider.GetRequiredService<GetGoogleAuthenticatorQRCodeService>();
+                var result = await service.ExecuteAsync(data);
+                return Ok(result);
+            }
+        }
+    }
+}
