14825cf0e3b6eb9b5e7ebe15f9c0ca025dc2d3f9
Marcos Krul <marcoskrul2k@gmail.com>
2024-07-09 20:44:27 -0300

    feat(models): add password history

---
+++ Migrations/20240709233504_AddPasswordHistory.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TucaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d476825b-cbf8-4412-b048-20d112e940f3");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fba69d46-cd31-450e-b77a-063e5645b6d3");
+
+            migrationBuilder.CreateTable(
+                name: "PasswordHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    AppUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PasswordHistory", x => new { x.AppUserId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_PasswordHistory_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "358f1394-be8e-4543-a2f4-636b9e1d7229", null, "Admin", "Admin" },
+                    { "e159014c-ff44-433a-b1a9-30cec1eddb3d", null, "User", "User" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PasswordHistory");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "358f1394-be8e-4543-a2f4-636b9e1d7229");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e159014c-ff44-433a-b1a9-30cec1eddb3d");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "d476825b-cbf8-4412-b048-20d112e940f3", null, "Admin", "Admin" },
+                    { "fba69d46-cd31-450e-b77a-063e5645b6d3", null, "User", "User" }
+                });
+        }
+    }
+}
--- src/Models/AppUser.cs
+++ src/Models/AppUser.cs
@@ -6,5 +6,6 @@ namespace TucaAPI.src.Models
     {
         public string? TwoFactorSecret { get; set; }
         public List<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
+        public List<PasswordHistory> PasswordHistory { get; set; } = new List<PasswordHistory>();
     }
 }
