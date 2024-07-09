31f4cc82d27da3ed19ac45ab56c0e6fca290098b
Marcos Krul <marcoskrul2k@gmail.com>
2024-07-09 20:19:29 -0300

    feat(services): update password service

--- src/Services/Account/UpdatePasswordService.cs
+++ src/Services/Account/UpdatePasswordService.cs
@@ -1,13 +1,50 @@
+using Microsoft.AspNetCore.Identity;
+using TucaAPI.Extensions;
+using TucaAPI.src.Common;
 using TucaAPI.src.Dtos.Account;
 using TucaAPI.src.Dtos.Common;
+using TucaAPI.src.Exceptions;
+using TucaAPI.src.Extensions;
+using TucaAPI.src.Mappers;
+using TucaAPI.src.Models;
+using TucaAPI.src.Utilities.Extensions;
 
 namespace TucaAPI.src.Services.Account
 {
     public class UpdatePasswordService : IService<UpdatePasswordRequestDto, ApiResponse>
     {
+        private readonly UserManager<AppUser> userManager;
+
+        public UpdatePasswordService(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
         public async Task<ApiResponse> ExecuteAsync(UpdatePasswordRequestDto data)
         {
-            throw new NotImplementedException();
+            var user = await this.userManager.FindNonNullableUserAsync(
+                data.GetNonNullableUser().GetEmail().GetNonNullable()
+            );
+
+            if (data.NewPassword == data.OldPassword)
+                throw new AppException(
+                    StatusCodes.Status400BadRequest,
+                    MessageKey.UPDATE_TO_SAME_PASSWORD
+                );
+
+            var result = await userManager.ChangePasswordAsync(
+                user,
+                data.OldPassword.GetNonNullable(),
+                data.NewPassword.GetNonNullable()
+            );
+
+            if (!result.Succeeded)
+                throw new AppException(
+                    StatusCodes.Status400BadRequest,
+                    result.Errors.Select(item => item.ToAppErrorDescriptor())
+                );
+
+            return new ApiResponse { Success = true };
         }
     }
 }
--- src/Utilities/Common/MessageKey.cs
+++ src/Utilities/Common/MessageKey.cs
@@ -21,5 +21,6 @@ namespace TucaAPI.src.Common
         public const string ERROR_UPDATE_USER = "ERROR_UPDATE_USER";
         public const string TWO_FACTOR_REQUIRED = "TWO_FACTOR_REQUIRED";
         public const string INVALID_TOKEN = "INVALID_TOKEN";
+        public const string UPDATE_TO_SAME_PASSWORD = "UPDATE_TO_SAME_PASSWORD";
     }
 }
